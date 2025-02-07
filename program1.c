ebcac66e622713742e7aa542f59535111eecedc5
SandorMore <msaso0725@gmail.com>
2025-02-07 16:20:27 +0100

    as

--- program1.c
+++ program1.c
@@ -3,5 +3,27 @@
 int main(){
 
     printf("Hello World\n");
+    findValueOfAll();
     return 0;
+}
+int datatypes(){
+    int age = 17;
+    float o = 4.213;
+    char grande = 'C';
+    unsigned char nig = 250;
+    char name[] = "Sanyi";
+    long long l = 9999999999999999999;
+    printf("Im %s\n", name);
+    printf("Im also %d", age);
+    printf(" old");
+    printf(" %c \n", nig);
+    printf("\n");
+    printf("Ez egy geci nagy szam  : %lld", l);
+}
+
+int findValueOfAll(){
+    float item1 = 55.3;
+    float item2 = 12.3234443;
+
+    printf("Item 1's price is : %f.1\n", item1);
 }
