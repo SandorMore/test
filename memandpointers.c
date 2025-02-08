247c1ce6aa2ad867374da49405f7f34c2f4c9568
SandorMore <msaso0725@gmail.com>
2025-02-08 19:37:00 +0100

    pointeslearn

--- memandpointers.c
+++ memandpointers.c
@@ -2,8 +2,30 @@
 
 int main(){
 
-    char a = 'A';
+    char address[9999];
+    short a[] = {'a','b','c', 'e'};
+    long long b = 401010101002;
+    short c = 'C';
 
+    printf("%d\n", sizeof(a));
+    printf("%d\n", sizeof(b));
+    printf("%d\n", sizeof(c));
+    printf("%d\n", sizeof(address));
+    printf("%p\n", &a);
+    printf("%p\n", &b);
+    printf("%p\n", &c);
+    printf("%p\n", &address);
 
+    printf("\n");
+    r();
     return 0;
+}
+void r(){
+    int age = 17;
+    int *pAge = &age;
+
+    printf("%d\n", sizeof(pAge));
+    printf("%p\n", &age);
+
+    printf("The value at the address is : %d\n", *pAge);
 }
