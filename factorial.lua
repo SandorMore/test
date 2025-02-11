138ad299b60dc7ff03256c577c2f8cb30d303bd6
SandorMore <msaso0725@gmail.com>
2025-02-11 21:36:18 +0100

    luanacci number

--- factorial.lua
+++ factorial.lua
@@ -5,5 +5,17 @@ local function fact(n)
         return n * fact(n-1)
     end
 end
-
+local memo = {}
+local function FiboNum(n)
+    if memo[n] then
+        return memo[n]
+    end
+    if n <= 2 then
+        return 1
+    end
+    local ans = FiboNum(n - 1) + FiboNum(n - 2)
+    memo[n] = ans
+    return ans
+end
 print(fact(5))
+print(FiboNum(50))
