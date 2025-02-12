464496c09c9407d7dd9cc7a10b9e04fd5df163a2
SandorMore <msaso0725@gmail.com>
2025-02-12 20:52:36 +0100

    kk

--- gyakorlas.c
+++ gyakorlas.c
@@ -9,16 +9,17 @@ struct User {
 int main(int argc, char *argv[]) {
     struct User user;
 
+    return 0;
+}
+void AskForName(struct User user){
     printf("What is your name?\n");
-    scanf("%63s", user.name); // Use %s to read a string and limit input to avoid buffer overflow
+    scanf("%63s", user.name);
     printf("What is your age?\n");
     scanf("%d", &user.age);
     printf("What is your occupation?\n");
-    scanf("%63s", user.occupation); // Use %s to read a string and limit input to avoid buffer overflow
+    scanf("%63s", user.occupation);
 
     printf("Name: %s\n", user.name);
     printf("Age: %d\n", user.age);
     printf("Occupation: %s\n", user.occupation);
-
-    return 0;
 }
