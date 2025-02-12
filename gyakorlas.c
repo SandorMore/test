0cfb507dd71c1302fe87abcc2bfd5b839ae834cf
SandorMore <msaso0725@gmail.com>
2025-02-12 15:31:43 +0100

    input buffer solved

--- gyakorlas.c
+++ gyakorlas.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
-
-struct  User
-{
+struct User {
     char name[64];
     int age;
     char occupation[64];
-
 };
 
-void main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     struct User user;
 
     printf("What is your name?\n");
-    scanf("%c", &user.name);
+    scanf("%63s", user.name); // Use %s to read a string and limit input to avoid buffer overflow
     printf("What is your age?\n");
     scanf("%d", &user.age);
     printf("What is your occupation?\n");
-    scanf("%c", &user.occupation);
-    
+    scanf("%63s", user.occupation); // Use %s to read a string and limit input to avoid buffer overflow
+
+    printf("Name: %s\n", user.name);
+    printf("Age: %d\n", user.age);
+    printf("Occupation: %s\n", user.occupation);
+
+    return 0;
 }
